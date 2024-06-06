db9a6cd6831f7edf29c072ea0ac2a59fbd59f9da
Murat <mur@sd.com>
2024-06-06 09:25:23 +0500

    last;

--- src/components/pages/lab.tsx
+++ src/components/pages/lab.tsx
@@ -2,9 +2,9 @@ const Lab = () => {
   return (
     <>
       <iframe
-        src="https://phet.colorado.edu/sims/html/energy-skate-park-basics/latest/energy-skate-park-basics_all.html"
+        src="https://phet.colorado.edu/sims/html/energy-skate-park/latest/energy-skate-park_all.html"
         width="100%"
-        height="650px"
+        height="610px"
       ></iframe>
     </>
   );
